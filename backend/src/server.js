7e3bbde47c83e49c4d0ef1da6f44cd7004706120
grkndev <gweepcreative@gmail.com>
2025-08-21 12:00:00 +0300

    cron

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,18 @@ import dotenv from "dotenv";
 import { initDb } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactions.route.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 const app = new Hono();
 
+if (process.env.NODE_ENV === "production") job.start();
+
 app.use("*", rateLimiter);
+
+app.get("/api/health", (c) => c.json({ status: "ok" }, 200));
 
 app.get("/status", (c) => c.json({ status: "ok" }, 200));
 app.route("/api/transactions", transactionsRoute);
